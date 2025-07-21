f7c4b3e3ca981e1c7e4f2362c3783b3839d36f9f
Federico Lozer <122794804+federicolozer@users.noreply.github.com>
2025-07-21 10:00:08 +0200

    21/07/25 - changed main for table

--- scripts/main.py
+++ scripts/main.py
@@ -272,7 +272,7 @@ def main(traj):
         controller_client(t_arm, q_arm, t_gripper, q_gripper, ttype, traj)
 
         name = traj.replace("_", "\_")
-        latex = f"\t${name}$ & {rmse:>1.2f} & {error:>1.2f} & {NN_time:>1.3f} & {IK_time:>1.3f} & {cnt}/{len(trajectory['waypoints'])}\\\\\n"
+        latex = f"\t${name}$ & {rmse:>1.2f} & {error:>1.2f} & {NN_time:>1.3f} & {IK_time:>1.3f} & {cnt}/{len(trajectory['waypoints'])}\\\\   %{traj}\n"
 
         file = open(f'{pack_path}/data/latex/table.txt', 'a')
         file.write(latex)
